c35aff3fc52161a505406b220e2b53a297d43ea0
czechbol <adamludes@gmail.com>
2021-06-20 16:24:12 +0200

    Added config file

--- src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
+++ src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
@@ -1,13 +1,19 @@
 package czechbol.dynamicchestshop;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DynamicChestShop extends JavaPlugin {
+    FileConfiguration config = getConfig();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("DynamicChestShop has started UP.");
 
+        //Setup Config
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
     }
 
     @Override
