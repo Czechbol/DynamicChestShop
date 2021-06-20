3ea6d1c2991b05f11dfc50e677bcda263a9af8a8
charashenko <charashenko@gmail.com>
2021-06-20 19:18:35 +0200

    Vault hook

--- src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
+++ src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
@@ -1,10 +1,13 @@
 package czechbol.dynamicchestshop;
 
 import czechbol.dynamicchestshop.staticshop.Handler;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import czechbol.dynamicchestshop.staticshop.ChestShop;
 import org.bukkit.Location;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -12,13 +15,18 @@ import java.util.HashMap;
 public final class DynamicChestShop extends JavaPlugin {
     FileConfiguration config = getConfig();
     public static HashMap<Location, ChestShop> shops = new HashMap<>();
-
+    private static Economy econ = null;
+    private static Permission perms = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("DynamicChestShop has started UP.");
         Bukkit.getServer().getPluginManager().registerEvents(new Handler(), this);
+
+        //Vault hooks
+        setupPermissions();
+        setupEconomy();
 
         //Setup Config
         getConfig().options().copyDefaults();
@@ -28,5 +36,31 @@ public final class DynamicChestShop extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public static Economy getEcon() {
+        return econ;
+    }
+
+    public static Permission getPerms() {
+        return perms;
     }
 }
