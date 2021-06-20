0213cbe9db26d4f188c3cec1b4619dcc463d2dbe
charashenko <charashenko@gmail.com>
2021-06-20 18:53:27 +0200

    Hooked vault

--- src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
+++ src/main/java/czechbol/dynamicchestshop/DynamicChestShop.java
@@ -1,15 +1,26 @@
 package czechbol.dynamicchestshop;
 
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DynamicChestShop extends JavaPlugin {
+
     FileConfiguration config = getConfig();
+
+    private static Economy econ = null;
+    private static Permission perms = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("DynamicChestShop has started UP.");
+
+        // Vault hooks
+        setupEconomy();
+        setupPermissions();
 
         //Setup Config
         getConfig().options().copyDefaults();
@@ -19,5 +30,31 @@ public final class DynamicChestShop extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    public static Permission getPermissions() {
+        return perms;
     }
 }
